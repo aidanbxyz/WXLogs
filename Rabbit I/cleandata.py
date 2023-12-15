709399071d5b4459e3822a75826d24a3db024172
aidanbxyz <43518094+aidanbxyz@users.noreply.github.com>
2023-12-15 15:25:23 -0600

    Add some R1 logs
    2023-08-12 002941-013845
    2023-08-13 231851-233311

---
+++ Rabbit I/cleandata.py
@@ -0,0 +1,28 @@
+logname = input('Log file: ')
+logfile = open(logname, 'r')
+lines = logfile.readlines()
+logfile.close()
+oldline = ''
+liveline = ''
+firstline = True
+logfile = open(logname + '.clean.log', 'a')
+for line in lines:
+    oldline = liveline
+    liveline = line.replace('[', '').replace(']', '').replace(',\n', '').split(',')
+    if liveline == ['f', 'da', 'db', 'dc', 'ga', 'go', 't', 'h', 'p']: # checks for signal
+        continue
+    for i in range(9): # fills in gaps in data
+        try:
+            liveline[i] = float(liveline[i])
+        except:
+            liveline[i] = oldline[i]
+        liveline[i] = str(liveline[i])
+    if liveline[4] == "0.0": # Checks for GPS lock
+        continue
+    if firstline:
+        logfile.write('[' + ','.join(liveline) + ']')
+        firstline = False
+    else:
+        logfile.write(',[' + ','.join(liveline) + ']')
+logfile.close()
+print('Saved as ' + logname + '.clean.log')
